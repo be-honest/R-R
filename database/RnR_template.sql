29a2b0c2789996f8db25dee78706178b1fa5c1c6
kemezike <kemartus@gmail.com>
2017-04-17 13:25:12 +0800

    Added database

---
+++ database/RnR_template.sql
@@ -0,0 +1,44 @@
+/*
+SQLyog Ultimate v12.3.2 (64 bit)
+MySQL - 10.1.21-MariaDB : Database - restandrecreation
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`restandrecreation` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `restandrecreation`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `email` varchar(120) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `profile_pic` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`uid`,`username`,`email`,`password`,`name`,`profile_pic`) values 
+(1,'kemezike','kemartus@gmail.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','Kryce Earl Martus',NULL),
+(2,'johnM','jandm@gmail.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','John Mikes',NULL),
+(3,'haguanta','ha@gmail.com','e6193a1d9da8b080746e08cb1c9c9225651e82315b8fffcbb2f87b8c1b93575d','Honest',NULL),
+(4,'kemezike123','Keme@gmail.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','Kryfce Earl',NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
