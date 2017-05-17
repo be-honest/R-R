34c7600014f13d99ba0f8eca1d5f7b491f7eddd2
be-honest <honestaguanta@gmail.com>
2017-05-17 16:47:48 +0800

    changes in event poll

---
+++ views/links/about.php
@@ -0,0 +1,33 @@
+<?php 
+	require_once 'views/layouts/header.php';
+	require_once 'views/layouts/nav.php';
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>About Us |</title>
+ 	<style>
+ 		
+ 		body{
+ 			display: flex;
+			align-items: center;
+			justify-content: center;
+ 		}
+		
+ 	</style>
+ </head>
+ <body>
+	<!-- <div class="row">
+		<div class="col-lg-12 text-center">
+			<h1>ABOUT</h1>
+			<hr>
+		</div>
+	</div> -->
+	<div class="hr-divider">
+		<h3 class="hr-divider-content hr-divider-heding">
+			About Us
+		</h3>
+	</div>
+ </body>
+ </html>
--- views/menubar/makeEvent.php
+++ views/menubar/makeEvent.php
@@ -55,68 +55,78 @@
 
 </style>
 <br>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 ">
-                <form method="post" name="createEvent" class="form-horizontal" role="form">
-                    <fieldset>
-                        <legend style="font-size: 50px;">Event Form  <span class="icon icon-new-message"></span></legend>
-                            <div class="form-group">
-                                <label class="control-label col-md-3">Name</label>
-                                    <div class="col-md-4">
-                                        <input type="input" id="event_name" placeholder="Event Name" class="form-control" name="event_name" value="Hiking" required autofocus>
-                                    </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-md-3 control-label" for="textarea">Description</label>
-                                    <div class="col-md-4">
-                                        <textarea class="form-control" id="textarea" name="description" required>HIKING!</textarea>
-                                    </div>
-                            </div> 
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 ">
+      <form method="post" name="createEvent" class="form-horizontal" role="form">
+        <fieldset>
+          <legend style="font-size: 50px;">Event Form  <span class="icon icon-new-message"></span></legend>
+              <div class="form-group">
+                  <label class="control-label col-md-2">Name</label>
+                      <div class="col-md-4">
+                          <input type="input" id="event_name" placeholder="Event Name" class="form-control" name="event_name" value="Hiking" required autofocus>
+                      </div>
+              </div>
+              <div class="form-group">
+                  <label class="col-md-2 control-label" for="textarea">Description</label>
+                      <div class="col-md-4">
+                          <textarea class="form-control" id="textarea" name="description" required>HIKING!</textarea>
+                      </div>
+              </div>
+              <!-- activity list -->
+                 <!-- <div class="form-group">
+                     <label class="col-md-2 control-label">Activities</label>
+                         <div class="col-md-2">
+                             <input type="text" id="activity" placeholder="Add an activity" class="form-control" name="activity[]">
+                         </div>
+                          <span onclick="newElement()" class="addBtn m-r-md span-control icon icon-circle-with-plus" style="font-size: x-large;"></span>
+                 </div>
+                 <div class="form-group">
+                     <label class="col-md-2 control-label"></label>
+                         <div class="col-md-4">
+                         
+                             <div class="panel panel-bold panel-info">
 
-                 <!-- activity list -->
-                    <!-- <div class="form-group">
-                        <label class="col-md-3 control-label">Activities</label>
-                            <div class="col-md-3">
-                                <input type="text" id="activity" placeholder="Add an activity" class="form-control" name="activity[]">
-                            </div>
-                             <span onclick="newElement()" class="addBtn m-r-md span-control icon icon-circle-with-plus" style="font-size: x-large;"></span>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-md-3 control-label"></label>
-                            <div class="col-md-4">
-                            
-                                <div class="panel panel-bold panel-info">
+                                 <ul style="list-style: none;" id="list" class="list">
+                                 </ul>
+                             </div>
+                             
+                         </div>
+                 </div> -->
+                 <!-- end of activity list -->
 
-                                    <ul style="list-style: none;" id="list" class="list">
-                                    </ul>
-                                </div>
-                                
-                            </div>
-                    </div> -->
-                    <!-- end of activity list -->
-
-                    <!-- check list -->
-                        <!-- <div class="form-group">
-                            <label class="col-md-3 control-label">Things to Bring</label>
-                                <div class="col-md">    
-                                    <div class="col-md-6">
-                                        <input type="text" data-role="tagsinput" class="tag form-control" placeholder="Items..." required>
-                                    </div>
-                                </div>
-                        </div> -->
-                    <!-- location -->
-                        <div class="form-group">
-                            <label class="col-md-3 control-label">Location</label>
-                                <div class="col-md-4"> 
-                                    <input type="url" class="form-control" name="location" value="http://localhost/RandR/Events.php" required>
-                                    <br>
-                                </div>
-                                  <br>                  
-                                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15702.131259526826!2d123.88905110000002!3d10.299175500000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7f08f824b1ab47f9!2sCoreDev+Solutions+Inc.!5e0!3m2!1sen!2sph!4v1494381300383" width="550" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
-                            </div>
-                        
+                 <!-- check list -->
+                     <!-- <div class="form-group">
+                         <label class="col-md-2 control-label">Things to Bring</label>
+                             <div class="col-md">    
+                                 <div class="col-md-6">
+                                     <input type="text" data-role="tagsinput" class="tag form-control" placeholder="Items..." required>
+                                 </div>
+                             </div>
+                     </div> --> 
+                  <div class="form-group">
+                    <label class="col-md-2 control-label">Location</label>
+                      <div class="col-md-4"> 
+                        <input type="url" class="form-control" name="location" value="http://localhost/RandR/Events.php" required>
                         <br>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15702.131259526826!2d123.88905110000002!3d10.299175500000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7f08f824b1ab47f9!2sCoreDev+Solutions+Inc.!5e0!3m2!1sen!2sph!4v1494381300383" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        
+                      </div>
+                        <br>
+                    
+                   </div>
+                   <div class="form-group">
+                      <label class="col-md-2 control-label">Upload Image</label>
+                         <div class="input-group">
+                           <span class="input-group-btn">
+                             <span class="btn btn-default btn-file" style="font-size: 14px;">
+                               Browse...<input type="file" id="imgInp">
+                             </span>
+                           </span>
+                           <input type="text" class="form-control" readonly style="width: 50%;">
+                         </div>
+                         <img id="img-upload">
+                    </div>
                     <!-- button -->
                     <div class="form-group">
                       <div class="col-sm-6 col-sm-offset-3">
@@ -153,7 +163,8 @@
                         </div>
                       <?php } ?>
                     </div>
-            <!-- modal -->
+                    <!-- end of button -->
+                          <!-- modal -->
                       <div class="modal fade" id="msg" tabindex="-1" role="dialog" aria-labelledby="msg" aria-hidden="true">
                         <div class="modal-dialog">
                           <div class="modal-content">
@@ -169,7 +180,6 @@
                             </div> -->
                             <div class="modal-body">
                                 <p>Do you wish to add an activity?</p>
-                            
                             <!-- <div class="modal-footer"> -->
                               <div style="display: flex; align-items: center; justify-content: space-around; ">
                                 <button class="btn btn-default" data-dismiss="modal">
@@ -185,17 +195,17 @@
                               </div>
                             </div>
                           </div>
-                      
                         </div>
                       </div>
-                  </div>
-                </div>
-              </div>
-            </fieldset>
-          </form>
-        </div>
-      </div>
+                    <!-- end of modal -->
+              
+        </fieldset>
+      </form>
     </div>
+  </div>
+</div> 
+                        
+          
     <?php 
         require_once 'views/layouts/footer.php';
      ?>
